9945115b6eb99ba82d442aae83f1d9bbb22d83b3
renguanying <ren.yingzi@163.com>
2025-03-19 17:51:39 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>app</div>
+  <div>{{$store.state.username}}</div>
+  <button type="submit" @click="mutationsFn"></button>
+  <button type="submit" @click="actionsFn"></button>
 </template>
+<script setup>
+import { useStore } from 'vuex'
+const store = useStore()
+console.log(store.state.moduleA.username)
+console.log(store.getters['moduleA/newName'])
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+const mutationsFn = () => {
+  // store.commit('updateName', 'ls')
+  store.commit('moduleA/updateName', 'll')
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+const actionsFn = () => {
+  // store.dispatch('updateName', 'ww')
+  store.dispatch('moduleA/updateName', 'ww')
 }
-</style>
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,34 @@
 import { createStore } from 'vuex'
-
-export default createStore({
+const moduleA = {
+  namespaced: true,
   state: {
+    username: 'zs'
   },
   getters: {
+    newName (state) {
+      return state.username + '!@'
+    }
   },
   mutations: {
+    updateName (state, payload) {
+      state.username = 'ls' + payload
+    }
   },
   actions: {
-  },
+    updateName (ctx, payload) {
+      setTimeout(() => {
+        ctx.commit('updateName', payload)
+      }, 1000)
+    }
+  }
+}
+const moduleB = {
+
+}
+
+export default createStore({
   modules: {
+    moduleA,
+    moduleB
   }
 })
